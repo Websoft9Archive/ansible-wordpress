08781a750f0864b04e99cd7521cdb4e14b256562
hliblack <hliblack@outlook.com>
2019-07-04 17:57:47 +0800

    change wp-dz

--- roles/wordpress-discuz/files/init.sh
+++ roles/wordpress-discuz/files/init.sh
@@ -1,14 +1,22 @@
-#!/bin/bash
+# #!/bin/bash
+
+old_password=$(cat /credentials/password.txt | awk 'NR==2' |awk -F ":" '{print $2}' )
+new_password=$(</dev/urandom tr -dc '12345!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c10)
+
+systemctl restart mysqld
+mysqladmin -uroot -p${old_password} -h 127.0.0.1 password $new_password
+mysqladmin -uroot -p${old_password} -h localhost password $new_password
+
+echo 'Databases root Password:'$new_password  > /credentials/password.txt
 
 password=`< /dev/urandom tr -dc 0-9-A-Z-a-z|head -c ${1:-10};echo`
 
 systemctl restart mysqld
 
+mysql -uroot -p$new_password -e "CREATE USER 'wordpress'@'localhost' IDENTIFIED BY '${password}';"
+mysql -uroot -p$new_password -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' WITH GRANT OPTION;"
 
-mysql -uroot -p123456 -e "CREATE USER 'wordpress'@'localhost' IDENTIFIED BY '${password}';"
-mysql -uroot -p123456 -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' WITH GRANT OPTION;"
-
-printf "username:wordpress\npassword:${password}\n" >> ~/password.txt
+printf "username:wordpress\npassword:${password}\n" >> /credentials/password.txt
 
 sed -i "s/wordpress_password/${password}/" /data/wwwroot/wordpress/wp-config.php
 
